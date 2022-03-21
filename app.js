85bca721d65b7d935fde627ab9c31d6c07665d1e
Klay Clarke <clarkeklay@gmail.com>
2022-03-21 12:21:28 -0400

    add middleware

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -6,8 +8,17 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var compression = require("compression");
+var helmet = require("helmet");
 
 var app = express();
+
+// set up mongoose connection
+let mongoose = require("mongoose");
+let mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error"));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -17,6 +28,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
