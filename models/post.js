7fc95c43a88c3a12ddb983f4f6e4cb6b879ac59e
Klay Clarke <clarkeklay@gmail.com>
2022-03-21 13:07:31 -0400

    remove timestamp req: false (may be false by default)

--- models/post.js
+++ models/post.js
@@ -6,12 +6,12 @@ let PostSchema = new Schema({
   body: { type: String, required: true, minlength: 1 },
   author: { type: Schema.Types.ObjectId, ref: "User" },
   published: { type: Boolean, default: true }, // allow user to state whether they'd want to archive a post for later date publishing
-  timestamp: { type: Date, required: false, default: Date.now() }, // if post is archived, timestamp will be the date of publishing
+  timestamp: { type: Date, default: Date.now() }, // if post is archived, timestamp will be the date of publishing
 });
 
 // virtual for post url
 PostSchema.virtual("url").get(function () {
-  return `/works/post/${this._id}`;
+  return `/posts/${this._id}`;
 });
 
 // export post model
