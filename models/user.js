7bead81e5962b5e9ea6070c7d2c2508382c3251f
Klay Clarke <clarkeklay@gmail.com>
2022-03-21 11:54:00 -0400

    create user model

---
+++ models/user.js
@@ -0,0 +1,19 @@
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+  firstname: { type: String, required: true, minlength: 1 },
+  lastname: { type: String, required: true, minlength: 1 },
+  username: { type: String, required: true, minlength: 1 },
+  password: { type: String, required: true, minlength: 1 },
+  admin: { type: Boolean, default: false },
+  posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+});
+
+// virtual for user post portfolio url
+UserSchema.virtual("url").get(function () {
+  return `/users/${this._id}`;
+});
+
+//export user model
+module.exports = mongoose.model("User", UserSchema);
