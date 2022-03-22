a0ede765c9c48a3dba229b80a73a473ba100a2cf
Klay Clarke <clarkeklay@gmail.com>
2022-03-22 13:39:28 -0400

    add restful api handling for user model (get, post, put, delete)

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,6 +15,21 @@ exports.api_user_list = function (req, res, next) {
     });
 };
 
+// // get specific user
+exports.api_user_get = function (req, res, next) {
+  async.parallel(
+    {
+      user: function (callback) {
+        User.findById(req.params.userid).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) return next(err);
+      res.send(results.user);
+    }
+  );
+};
+
 // // create user
 exports.api_user_create = [
   // validate and sanitize fields
@@ -70,3 +85,29 @@ exports.api_user_delete = function (req, res, next) {
 };
 
 // update user
+exports.api_user_update = [
+  // validate and sanitize fields
+  body("firstname").trim().isLength({ min: 1 }).escape(),
+  body("lastname").trim().isLength({ min: 1 }).escape(),
+  body("username").trim().isLength({ min: 1 }).escape(),
+  body("admin"),
+  // process request
+  (req, res, next) => {
+    const errors = validationResult(req);
+    let user = new User({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      username: req.body.username,
+      admin: req.body.admin,
+      _id: req.params.userid,
+    });
+    if (!errors.isEmpty()) {
+      res.send(errors.array());
+    } else {
+      User.findByIdAndUpdate(req.params.userid, user, {}, (err, theuser) => {
+        if (err) return next(err);
+        res.send("User successfully updated");
+      });
+    }
+  },
+];
