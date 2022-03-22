8b053ea9e79d6c11c8e80ffcb83849f8063f31e1
Klay Clarke <clarkeklay@gmail.com>
2022-03-22 16:39:27 -0400

    add an api key to the end of each route - serves as a makeshift authorization in which a person must pass in a passcode in order to access the restful routes

--- routes/api.js
+++ routes/api.js
@@ -7,38 +7,77 @@ let comment_controller = require("../controllers/commentController");
 
 // USER MODEL ROUTES
 
-router.get("/users", user_controller.api_user_list);
+router.get(`/users/${process.env.BLOG_API_KEY}`, user_controller.api_user_list);
 
-router.get("/users/:userid", user_controller.api_user_detail);
+router.get(
+  `/users/:userid/${process.env.BLOG_API_KEY}`,
+  user_controller.api_user_detail
+);
 
-router.post("/users", user_controller.api_user_create);
+router.post(
+  `/users/${process.env.BLOG_API_KEY}`,
+  user_controller.api_user_create
+);
 
-router.put("/users/:userid", user_controller.api_user_update);
+router.put(
+  `/users/:userid/${process.env.BLOG_API_KEY}`,
+  user_controller.api_user_update
+);
 
-router.delete("/users/:userid", user_controller.api_user_delete);
+router.delete(
+  `/users/:userid/${process.env.BLOG_API_KEY}`,
+  user_controller.api_user_delete
+);
 
 // POST MODEL ROUTES
 
-router.get("/posts", post_controller.api_post_list);
+router.get(`/posts/${process.env.BLOG_API_KEY}`, post_controller.api_post_list);
 
-router.get("/posts/:postid", post_controller.api_post_detail);
+router.get(
+  `/posts/:postid/${process.env.BLOG_API_KEY}`,
+  post_controller.api_post_detail
+);
 
-router.post("/posts", post_controller.api_post_create);
+router.post(
+  `/posts/${process.env.BLOG_API_KEY}`,
+  post_controller.api_post_create
+);
 
-router.put("/posts/:postid", post_controller.api_post_update);
+router.put(
+  `/posts/:postid/${process.env.BLOG_API_KEY}`,
+  post_controller.api_post_update
+);
 
-router.delete("/posts/:postid", post_controller.api_post_delete);
+router.delete(
+  `/posts/:postid/${process.env.BLOG_API_KEY}`,
+  post_controller.api_post_delete
+);
 
 // COMMENT MODEL ROUTES
 
-router.get("/comments", comment_controller.api_comment_list);
+router.get(
+  `/comments/${process.env.BLOG_API_KEY}`,
+  comment_controller.api_comment_list
+);
 
-router.get("/comments/:commentid", comment_controller.api_comment_detail);
+router.get(
+  `/comments/:commentid/${process.env.BLOG_API_KEY}`,
+  comment_controller.api_comment_detail
+);
 
-router.post("/comments", comment_controller.api_comment_create);
+router.post(
+  `/comments/${process.env.BLOG_API_KEY}`,
+  comment_controller.api_comment_create
+);
 
-router.put("/comments/:commentid", comment_controller.api_comment_update);
+router.put(
+  `/comments/:commentid/${process.env.BLOG_API_KEY}`,
+  comment_controller.api_comment_update
+);
 
-router.delete("/comments/:commentid", comment_controller.api_comment_delete);
+router.delete(
+  `/comments/:commentid/${process.env.BLOG_API_KEY}`,
+  comment_controller.api_comment_delete
+);
 
 module.exports = router;
