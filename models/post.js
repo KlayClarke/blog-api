df58868a9a71d59863ac2183b28deac141f05d8b
Klay Clarke <clarkeklay@gmail.com>
2022-03-22 15:25:53 -0400

    change key label in post schema

--- models/post.js
+++ models/post.js
@@ -5,11 +5,10 @@ let PostSchema = new Schema({
   title: { type: String, required: true, minlength: 1 },
   author: { type: Schema.Types.ObjectId, ref: "User" },
   body_paragraphs: [{ type: String, required: true, minlength: 1 }],
-  body_images: [{ type: , required: false }],
+  images: [{ type: String, required: false }],
   published: { type: Boolean, default: true }, // allow user to state whether they'd want to archive a post for later date publishing
   timestamp: { type: Date, default: Date.now() }, // if post is archived, timestamp will be the date of publishing
 });
-
 
 // virtual for post url
 PostSchema.virtual("url").get(function () {
