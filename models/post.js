683e1fb831a53150430d0c905577a1c990d396c5
Klay Clarke <clarkeklay@gmail.com>
2022-03-21 20:05:27 -0400

    fields to post model schema (body paragraphs - a field that will allow me to separate the paragraphs in a blog post for formatting ease; body images - a field that will allow me to separate the images)

--- models/post.js
+++ models/post.js
@@ -3,11 +3,13 @@ let Schema = mongoose.Schema;
 
 let PostSchema = new Schema({
   title: { type: String, required: true, minlength: 1 },
-  body: { type: String, required: true, minlength: 1 },
-  author: { type: Schema.Types.ObjectId, ref: "Author" },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  body_paragraphs: [{ type: String, required: true, minlength: 1 }],
+  body_images: [{ type: , required: false }],
   published: { type: Boolean, default: true }, // allow user to state whether they'd want to archive a post for later date publishing
   timestamp: { type: Date, default: Date.now() }, // if post is archived, timestamp will be the date of publishing
 });
+
 
 // virtual for post url
 PostSchema.virtual("url").get(function () {
